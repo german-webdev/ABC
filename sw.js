a215a7a9f68cd517b76d66163689d2249238a56e
Pavel German <frontendue@gmail.com>
2023-10-03 23:13:57 +1000

    setting PWA

--- sw.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-importScripts('sw-toolbox.js'); 
-toolbox.precache(['index.html','dist/style.css', 'dist/app.js', 'dist/modal.js']); 
-toolbox.router.get('dist/images/*', toolbox.cacheFirst); 
-toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
