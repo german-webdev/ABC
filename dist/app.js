6a622bdce826e3f15aa036dafefe900301477e76
Pavel German <frontendue@gmail.com>
2023-10-04 00:31:14 +1000

    setting PWA

--- dist/app.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register("./dist/sw.js")
-    .then((registration) => console.log('ServiceWorker registration successful with scope: ', registration.scope))
-    .catch((error) => console.log('Registration failed', error))
-}
