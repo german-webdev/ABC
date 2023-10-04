976d5004ce82d4e10c54ca3ce19ec9a7ed0c6b5c
Pavel German <frontendue@gmail.com>
2023-10-04 20:36:00 +1000

    fixed PWA

--- vite.config.js
+++ vite.config.js
@@ -18,17 +18,18 @@ const vitePWA = VitePWA({
     name: "ABC-Games",
     short_name: "ABC",
     display: "standalone",
+    start_url: "https://abc-a2s55kbsw-german-webdev.vercel.app",
     description: "Игра для зарядки мозга «Алфавит»",
     theme_color: "#ffffff",
     background_color: "#ffffff",
     icons: [{
-      src: 'assets/images/android-chrome-192x192.png',
+      src: '/assets/images/android-chrome-192x192.png',
       sizes: '192x192',
       type: 'image/png',
 
     },
     {
-      src: 'assets/images/android-chrome-512x512.png',
+      src: '/assets/images/android-chrome-512x512.png',
       sizes: '512x512',
       type: 'image/png',
       
@@ -63,6 +64,11 @@ export default defineConfig({
         },
       },
     },
+  },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    hmr: true,
   },
   plugins: [
     ViteAliases(),
